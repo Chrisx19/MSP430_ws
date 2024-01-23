b8c251fcbd490bbc0202601e8b8a8569507c8ecc
Chrisx19 <christopherlazo1619@gmail.com>
2024-01-22 21:47:09 -0800

    final_led_test

--- main.c
+++ main.c
@@ -9,9 +9,12 @@ int main(void)
     PM5CTL0 &= ~LOCKLPM5;
 	WDTCTL = WDTPW | WDTHOLD;	// stop watchdog timer
 
-	//Setting P1 as output config
+	//Setting P1 for 0th bit as output config
 	P1DIR |= 0x01;
+	//Setting P4 for 0th bit as output config
 	P4DIR |= 0x01;
+
+	//Setting 0th LED of port 1 and port 4 to high
 	P1OUT |= 0x01;
 	P4OUT |= 0x01;
 
