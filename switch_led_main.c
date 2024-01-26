6df4e6bfcd702ac2040982ee4a119eaf0eb05788
Chris Lazo <107272321+Chrisx19@users.noreply.github.com>
2024-01-25 21:33:51 -0800

    button switch with ISR and LED

--- switch_led_main.c
+++ switch_led_main.c
@@ -1,38 +1,65 @@
-#include <msp430.h> 
+#include <msp430.h>
+#include <stdint.h>
 
-
+uint8_t timerCount = 0;
+int code = 0;
 /**
  * main.c
  */
+
+void init(void) {
+    PM5CTL0 &= ~LOCKLPM5;
+    WDTCTL = WDTPW | WDTHOLD;   // stop watchdog timer
+}
+
+
+void clkInit(void)
+{
+    TA0CTL |= 0x0100; //0000 0001 0000 0000 SETTING ACLK ~ TASSEL
+    TA0CTL |= 0x0010; //0000 0001 0001 0000 SETTING UP MODE which correspond to TAXCCR0
+    TA0CTL |= 0x0004; //0000 0001 0001 0100 CLEAR; automatic reset when counter is up max
+
+    TA0CCR0 = 4096; //Set 32768/8=4096 to set the pulse faster; for Capture & control threshold
+    TA0CCTL0 |= 0x0010; //enables ISR on capture & compare mode
+
+}
+
+void switchInit(void)
+{
+    P1DIR &= ~0x04; //bit2
+    P1REN |= 0x04; //enable pull up or pulldown
+    P1OUT |= 0x04; //pull up, so its always on or '1'
+}
+
 int main(void)
 {
-    PM5CTL0 &= ~LOCKLPM5;
-	WDTCTL = WDTPW | WDTHOLD;	// stop watchdog timer
-
-    // Configure P1.1 as input for the button
-    P1DIR &= ~BIT1;  // Set P1.1 as input
-    P4DIR &= ~BIT5;  // Set P4.5 as input
-
-    P1REN |= 0x02;   // Enable pull-up/pull-down resistor
-    P4REN |= 0x20;
-    P1OUT |= 0x02;   // Select pull-up mode
-    P4OUT |= 0x20;
+    init();
+    clkInit();
 
     // Configure P1.0 as output for LED1
     P1DIR |= 0x01;  // //BIT0
-    P4DIR |= 0x40; //0100 0000 Set P4.6 to output direction
+    P4DIR |= 0x01; //0100 0000 Set P4.6 to output direction
 
-    while(1) {
-        if((P1IN & BIT1) != BIT1) {  // Check if button is pressed
-            P1OUT |= 0x01;    // Turn on LED1
+    __bis_SR_register(LPM0_bits + GIE);
+}
 
-        }
-        else if (!(P4IN & 0x20)) {
-            P4OUT |= 0x40;    //BIT6
-        } else {
-            P1OUT &= ~0x01;   // Turn off LED1
-            P4OUT &= ~0x40;
+
+
+#pragma vector=TIMER0_A0_VECTOR
+__interrupt void TIMER0_A0_ISR(void)
+{
+    timerCount++;
+    if ((P1IN & 0x04) != 0x04) {
+        code ^= 1;
+    }
+
+    if(code) {
+        if (timerCount >= 8)  {
+                timerCount = 0;
+                P1OUT ^= 0x01;
+                P4OUT ^= 0x01;
         }
     }
-	return 0;
+
+
 }
