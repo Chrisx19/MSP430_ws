b629c69f70af13eec239438c7ec3f632ac3425e2
Chris Lazo <107272321+Chrisx19@users.noreply.github.com>
2024-01-26 11:28:17 -0800

    Update and rename switch_led_main.c to switch_led_main_5969.c

--- switch_led_main_5969.c
+++ switch_led_main_5969.c
@@ -14,9 +14,9 @@ void init(void) {
 
 void switchInit(void)
 {
-    P1DIR &= ~0x04; //bit2
-    P1REN |= 0x04; //enable pull up or pulldown
-    P1OUT |= 0x04; //pull up, so its always on or '1'
+    P1DIR &= ~0x02; //bit2
+    P1REN |= 0x02; //enable pull up or pulldown
+    P1OUT |= 0x02; //pull up, so its always on or '1'
 }
 
 int main(void)
@@ -26,12 +26,15 @@ int main(void)
 
     // Configure P1.0 as output for LED1
     P1DIR |= 0x01;  // //BIT0
-    P4DIR |= 0x01; //0100 0000 Set P4.6 to output direction
+    P4DIR |= 0x40; //0100 0000 Set P4.6 to output direction
 
     while(1) {
-        if ((P1IN & 0x04) != 0x04) {
+        if ((P1IN & 0x02) != 0x02) {
             P1OUT ^= 0x01;
             P4OUT ^= 0x40;
+        } else {
+            P1OUT = P1OUT;
+            P4OUT = P4OUT;
         }
     }
 }
