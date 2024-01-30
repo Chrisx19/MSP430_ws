3a604d3953557bafbfcfd8df3c128d4b7fa46ac6
Chris Lazo <107272321+Chrisx19@users.noreply.github.com>
2024-01-30 10:24:36 -0800

    Update main.c

--- i2c_INA219/main.c
+++ i2c_INA219/main.c
@@ -10,48 +10,25 @@
     #define SDA BIT6
 #endif
 
-
 volatile uint8_t txBuffer[2];
 volatile uint8_t txByteCount = 0;  // Counter for transmission bytes
-volatile uint8_t rxData[2];
+volatile uint8_t rxBuffer[2];
 
-void i2cInit(void){
-    // Configure GPIO
-    P5SEL0 |= SCL;
-    P5SEL0 |= SDA;
-
-    // USCI_B0
-    UCB0CTLW0 |= UCSWRST;                     // Software reset enabled
-    UCB0CTLW0 |= UCSSEL_3;                    // SMCLK
-    UCB0CTLW0 |= UCMODE_3 | UCMST;            // I2C mode, Master mode
-    UCB0CTLW0 |= UCTR;                        // Transmit Mode
-    UCB0CTLW1 |= UCASTP_2;                    // Automatic stop generated
-
-    UCB0BRW = 10;                             // Baudrate
-    UCB0TBCNT = sizeof(txBuffer);             // number of bytes to send
-    UCB0I2CSA = INA219_ADDRESS;               // Slave address
-    UCB0CTLW0 &= ~UCSWRST;
-    UCB0IE |= UCTXIE;
-    __enable_interrupt();
-}
-
-void transmitRegister(uint16_t const config) {
-    uint8_t const TX_SIZE = sizeof(txBuffer)/sizeof(uint8_t);
-    memset((uint8_t *)txBuffer, 0x00, TX_SIZE);
-    txBuffer[0] = (config >> 8) & 0xff;  // MSB
-    txBuffer[1] = config & 0xff;         // LSB
-    txByteCount = 0;                     // Reset byte counter
-    UCB0CTLW0 |= UCTXSTT;                // I2C TX, start condition
-}
+void i2cInit(void);
+void TransmitRegister(uint16_t const config);
 
 //not done
-void Receive(void){
-   UCB0CTL1 &= ~UCTR ;                  // Clear UCTR
-   while (UCB0CTL1 & UCTXSTP);             // Ensure stop condition got sent
-   UCB0CTL1 |= UCTXSTT;                    // I2C start condition
-   while (UCB0CTL1 & UCTXSTT);             // Start condition sent?
-   UCB0CTL1 |= UCTXSTP;                    // I2C stop condition
-   __bis_SR_register(CPUOFF + GIE);        // Enter LPM0 w/ interrupts
+uint16_t ReceiveRegister(uint8_t const readRegister){
+    uint16_t rawData = 0x0000;
+    UCB0CTL1 &= ~UCTR ;                     // Clear UCTR
+    while (UCB0CTL1 & UCTXSTP);             // Ensure stop condition got sent
+    UCB0CTL1 |= UCTXSTT;                    // I2C start condition
+    while (UCB0CTL1 & UCTXSTT);             // Start condition sent?
+    UCB0CTL1 |= UCTXSTP;                    // I2C stop condition
+//    __bis_SR_register(CPUOFF + GIE);        // Enter LPM0 w/ interrupts
+
+    uint16_t raw = ((rxBuffer[0] << 8) | rxBuffer[1]);
+    return rawData;
 }
 
 int main(void)
@@ -70,8 +47,8 @@ int main(void)
 
   uint16_t const calibrationValue = 8192;
 
-  transmitRegister(config);
-  transmitRegister(calibrationValue);
+  TransmitRegister(config);
+  TransmitRegister(calibrationValue);
 
   while (1)
   {
@@ -102,8 +79,7 @@ __interrupt void USCI_B0_ISR(void)
     case USCI_I2C_UCRXIFG0:                 // Vector 22: RXIFG0
       break;
     case USCI_I2C_UCTXIFG0:                 // Vector 24: TXIFG0
-        P1OUT ^= BIT0;
-        if (txByteCount <= 2) {
+        if (txByteCount < 2) {
             while (UCB0CTL1 & UCTXSTP);             // Ensure stop condition was sent
             UCB0TXBUF = txBuffer[txByteCount++];    // Send first byte
             while (!(UCB0IFG & UCTXIFG));           // Wait for TX buffer ready
@@ -121,4 +97,40 @@ __interrupt void USCI_B0_ISR(void)
     case USCI_I2C_UCBIT9IFG: break;         // Vector 30: 9th bit
     default: break;
   }
+}
+
+void TransmitRegister(uint16_t const config)
+{
+    uint8_t const TX_SIZE = sizeof(txBuffer)/sizeof(uint8_t);
+    memset((uint8_t *)txBuffer, 0x00, TX_SIZE);
+    txBuffer[0] = (config >> 8) & 0xff;  // MSB
+    txBuffer[1] = config & 0xff;         // LSB
+    txByteCount = 0;                     // Reset byte counter
+    UCB0CTLW0 |= UCTXSTT;                // I2C TX, start condition
+}
+
+void i2cInit(void)
+{
+    // Configure GPIO
+#ifdef __MSP430FR4133__
+    P5SEL0 |= SCL;
+    P5SEL0 |= SDA;
+#elif defined(__MSP430FR5969__)
+    P1SEL1 |= SCL;
+    P1SEL1 |= SDA;
+#endif
+
+    // USCI_B0
+    UCB0CTLW0 |= UCSWRST;                     // Software reset enabled
+    UCB0CTLW0 |= UCSSEL_3;                    // SMCLK
+    UCB0CTLW0 |= UCMODE_3 | UCMST;            // I2C mode, Master mode
+    UCB0CTLW0 |= UCTR;                        // Transmit Mode
+    UCB0CTLW1 |= UCASTP_2;                    // Automatic stop generated
+
+    UCB0BRW = 10;                             // Baudrate
+    UCB0TBCNT = sizeof(txBuffer);             // number of bytes to send
+    UCB0I2CSA = INA219_ADDRESS;               // Slave address
+    UCB0CTLW0 &= ~UCSWRST;
+    UCB0IE |= UCTXIE;
+    __enable_interrupt();
 }
