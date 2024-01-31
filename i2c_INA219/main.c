142f8f2b185e5f4b68aeca94e702c52df11e1986
Chris Lazo <107272321+Chrisx19@users.noreply.github.com>
2024-01-30 21:50:57 -0800

    DONE_WORKS__Still need to optimize tho

--- i2c_INA219/main.c
+++ i2c_INA219/main.c
@@ -3,6 +3,9 @@
 #include "init.h"
 #include "ina219.h"
 
+#define TX_SIZE 3
+#define RX_SIZE 2
+
 #ifdef __MSP430FR4133__
     #define SCL BIT3
     #define SDA BIT2
@@ -11,37 +14,43 @@
     #define SDA BIT6
 #endif
 
-volatile uint8_t txBuffer[2];
-uint8_t txByteCount = 0;  // Counter for transmission bytes
+volatile uint8_t txBuffer[TX_SIZE];
+volatile uint8_t rxBuffer[RX_SIZE];
 
-volatile unsigned int rxRawIn = 0;
-volatile uint8_t rxBuffer[2];
+uint8_t txByteCount = 0;
 uint8_t rxByteCount = 0;
-
-uint16_t rawData_g = 0x0000;
 uint8_t readRegister_g = 0;
+uint16_t const CALIBRATION_VALUE = 8192; //0x2000
 
-void i2cInit(void);
-void TransmitRegister(uint16_t const config);
+float temp_g = 0;
 uint8_t ReceiveFLAG;
 
-//not done
-uint16_t ReceiveRegister(uint8_t const readRegister){
-    ReceiveFLAG = 1;
-    UCB0CTLW0 |= UCTR;
-    UCB0TBCNT = 1;
-    readRegister_g = readRegister;
-    UCB0CTLW0 |= UCTXSTT;
-    __delay_cycles(1000);
-//==========================================================================================//
-    UCB0CTLW0 &= ~UCTR;
-    UCB0TBCNT = 2;
-    UCB0CTLW0 |= UCTXSTT;
+void i2cInit(void);
+void TransmitRegister(uint8_t const reg, uint16_t const config, uint8_t const size);
+void ReceiveRegister(uint8_t const readRegister);
+void setCalibration_16V_400mA(void);
 
-    while ((UCB0IFG & UCSTPIFG) == 0);
-    UCB0IFG &= ~UCSTPIFG;
+uint16_t getCurrent_raw(void)
+{
+    uint16_t rawData_g = 0x0000;
+    TransmitRegister(INA219_REG_CALIBRATION, CALIBRATION_VALUE, TX_SIZE);
+
+    ReceiveRegister(INA219_REG_CURRENT);
     rawData_g = ((rxBuffer[0] << 8) | rxBuffer[1]);
     return rawData_g;
+}
+
+float getCurrent_mA(void)
+{
+    uint32_t ina219_currentDivider_mA = 20;
+    float valueDec = getCurrent_raw();
+
+    if (valueDec < 0) {
+        valueDec = 0;
+    } else {
+        valueDec /= ina219_currentDivider_mA;
+    }
+    return valueDec;
 }
 
 int main(void)
@@ -52,21 +61,14 @@ int main(void)
   disableInit();
   i2cInit();
   __enable_interrupt();                // Enables global interrupts
-  uint16_t const CONFIG =   SHUNT_BUS_CONT | SADC_12BIT_532US | BADC_12BIT_532US |
-                                      PG_GAIN_1_40mV | BRNG_FSR_16V;
-  uint16_t const CALIBRATION_VALUE = 8192; //0x2000
 
-  TransmitRegister(CONFIG);
-  __delay_cycles(1000);
-  TransmitRegister(CALIBRATION_VALUE);
-  __delay_cycles(1000);
-
+  setCalibration_16V_400mA();
 
   while (1)
   {
-      int test = ReceiveRegister(INA219_REG_CURRENT); //0x04
-      __delay_cycles(1000);
-
+      temp_g = getCurrent_mA();
+      P1OUT ^= BIT0;
+      __delay_cycles(2000000);
   }
 }
 
@@ -78,18 +80,20 @@ __interrupt void USCI_B0_ISR(void)
   {
     case USCI_I2C_UCRXIFG0:                 // Vector 22: RXIFG0
         rxBuffer[rxByteCount++] = UCB0RXBUF;
-
-        if ( rxByteCount >= 2 ) {
+        if ( rxByteCount >= RX_SIZE ) {
             rxByteCount = 0;
         }
         break;
+    // TODO: Need to update this
     case USCI_I2C_UCTXIFG0:                 // Vector 24: TXIFG0
         if (ReceiveFLAG == 0) {
-            if (txByteCount < 2) {
+            if (txByteCount < TX_SIZE) {
                 while (UCB0CTLW0 & UCTXSTP);         // Ensure stop condition was sent
                 UCB0TXBUF = txBuffer[txByteCount++]; // Send first byte
                 while ((UCB0IFG & UCTXIFG) == 0);    // Wait for TX buffer ready
-                UCB0TXBUF = txBuffer[txByteCount];   // Send second byte
+                UCB0TXBUF = txBuffer[txByteCount++]; // Send second byte
+                while ((UCB0IFG & UCTXIFG) == 0);    // Wait for TX buffer ready
+                UCB0TXBUF = txBuffer[txByteCount];   // Send third byte
             }
         } else {
             while (UCB0CTLW0 & UCTXSTP);             // Ensure stop condition was sent
@@ -101,18 +105,36 @@ __interrupt void USCI_B0_ISR(void)
   }
 }
 
-void TransmitRegister(uint16_t const config)
+void TransmitRegister(uint8_t const reg, uint16_t const config, uint8_t const size)
 {
     ReceiveFLAG = 0;
     UCB0CTLW0 |= UCTR;                   // Transmit Mode
-    uint8_t const TX_SIZE = sizeof(txBuffer)/sizeof(uint8_t);
-    memset((uint8_t *)txBuffer, 0x00, TX_SIZE);
+    memset((uint8_t *)txBuffer, 0x00, size);
 
-    UCB0TBCNT = sizeof(txBuffer);        // number of bytes to send
-    txBuffer[0] = (config >> 8) & 0xff;  // MSB
-    txBuffer[1] = config & 0xff;         // LSB
+    UCB0TBCNT = size;                   // number of bytes to send
+    txBuffer[0] = reg;
+    txBuffer[1] = (config >> 8) & 0xff;  // MSB
+    txBuffer[2] = config & 0xff;         // LSB
     txByteCount = 0;                     // Reset byte counter
     UCB0CTLW0 |= UCTXSTT;                // I2C TX, start condition
+    __delay_cycles(100);
+}
+
+void ReceiveRegister(uint8_t const readRegister)
+{
+    ReceiveFLAG = 1;
+    UCB0CTLW0 |= UCTR;
+    UCB0TBCNT = 1;
+    readRegister_g = readRegister;
+    UCB0CTLW0 |= UCTXSTT;
+    __delay_cycles(100);
+//==========================================================================================//
+    UCB0CTLW0 &= ~UCTR;
+    UCB0TBCNT = 2;
+    UCB0CTLW0 |= UCTXSTT;
+
+    while ((UCB0IFG & UCSTPIFG) == 0);
+    UCB0IFG &= ~UCSTPIFG;
 }
 
 void i2cInit(void)
@@ -138,4 +160,13 @@ void i2cInit(void)
     UCB0CTLW0 &= ~UCSWRST;               // Software reset disabled
 
     UCB0IE |= UCTXIE | UCRXIE;           // Enable ISR for Tx, and Rx; This goes to ISR routines
+}
+
+void setCalibration_16V_400mA(void)
+{
+    TransmitRegister(INA219_REG_CALIBRATION, CALIBRATION_VALUE, TX_SIZE);
+
+    uint16_t const CONFIG =   SHUNT_BUS_CONT | SADC_12BIT_532US | BADC_12BIT_532US |
+                                        PG_GAIN_1_40mV | BRNG_FSR_16V;
+    TransmitRegister(INA219_REG_CONFIG, CONFIG, TX_SIZE);
 }
