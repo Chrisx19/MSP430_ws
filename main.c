b879eb02cf4c43122e5a159f86dad3f184479274
Chris Lazo <107272321+Chrisx19@users.noreply.github.com>
2024-12-31 18:35:21 -0800

    Update main.c

--- main.c
+++ main.c
@@ -1,23 +1,26 @@
-#include <msp430.h> 
-
-
-/**
- * main.c
- */
+#include <msp430.h>
+#include "driverlib.h"
+/*
+This code example if want to read real measurement of Source clock such as SMCLK at this case. P3.4 is the output of the clock that can be measured with logic analyzer. 
+This code only works for MSP430FR5969, now if different board the need to read its own pins datasheet
+*/
 int main(void)
 {
-    PM5CTL0 &= ~LOCKLPM5;
-	WDTCTL = WDTPW | WDTHOLD;	// stop watchdog timer
+    WDT_A_hold(WDT_A_BASE);        // Stop watchdog
+    PMM_unlockLPM5();         // Unlock GPIO from LPMx.5
 
-	//Setting P1 for 0th bit as output config
-	P1DIR |= 0x01;
-	//Setting P4 for 0th bit as output config
-	P4DIR |= 0x01;
+    // Set DCO = 1 MHz
+    CS_setDCOFreq(CS_DCORSEL_0, CS_DCOFSEL_0);
+    // SMCLK = DCO @ 1 MHz, no division
+    CS_initClockSignal(CS_SMCLK, CS_DCOCLK_SELECT, CS_CLOCK_DIVIDER_1);
 
-	//Setting 0th LED of port 1 and port 4 to high
-	P1OUT |= 0x01;
-	P4OUT |= 0x01;
+    // P3.4 -> SMCLK output
+    GPIO_setAsPeripheralModuleFunctionOutputPin(GPIO_PORT_P3,
+                                                GPIO_PIN4,
+                                                GPIO_SECONDARY_MODULE_FUNCTION);
 
-	
-	return 0;
+    while(1)
+    {
+        __no_operation();  // SMCLK should appear on P3.4
+    }
 }
