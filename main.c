e943e7314b9ec094034c366f23fac1b7ace421f6
Chris Lazo <107272321+Chrisx19@users.noreply.github.com>
2025-01-06 16:03:55 -0800

    Update main.c

--- main.c
+++ main.c
@@ -1,166 +1,183 @@
-#include <msp430.h>
-#include <driverlib.h>
-#include <string.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdarg.h>
+#include "driverlib.h"
+#include "EVR/evr.h"
+#include <stdint.h>
 
-#define UART_BUFFER_SIZE 128
+EVR_t evr = {0};
+uint16_t tempADCCombined = 0;
+void sendCalibrationConstants();
+void ADC_Init(void);
+void Init_Clock();
 
-// UART configuration parameters
-// Datasheet to configure baudrate and UART
-// https://www.ti.com/lit/ug/slau367p/slau367p.pdf?ts=1706206110916&ref_url=https%253A%252F%252Fwww.ti.com%252Fproduct%252FMSP430FR5969
-EUSCI_A_UART_initParam uartConfig = {
-    EUSCI_A_UART_CLOCKSOURCE_SMCLK,    // Clock source: SMCLK
-    103,                              // Clock prescaler
-    0,                                // First modulation stage
-    0xDD,                             // Second modulation stage; 0.7503 (Pg 779)
-    EUSCI_A_UART_NO_PARITY,           // No parity
-    EUSCI_A_UART_LSB_FIRST,           // LSB first
-    EUSCI_A_UART_ONE_STOP_BIT,        // One stop bit
-    EUSCI_A_UART_MODE,                // UART mode
-};
+void Init_Clock();
 
-typedef enum {
-    EVR_SUCCESS = 0,
-    EVR_NULL_ERROR,
-    EVR_ERROR
-} EVR_Code;
+void GetAdcValue()
+{
+    // Start timer
+    Timer_A_initUpModeParam param = {0};
+    param.clockSource = TIMER_A_CLOCKSOURCE_ACLK;
+    param.clockSourceDivider = TIMER_A_CLOCKSOURCE_DIVIDER_1;
+    param.timerPeriod = 13;
+    param.timerInterruptEnable_TAIE = TIMER_A_TAIE_INTERRUPT_DISABLE;
+    param.captureCompareInterruptEnable_CCR0_CCIE = TIMER_A_CCIE_CCR0_INTERRUPT_ENABLE;
+    param.timerClear = TIMER_A_DO_CLEAR;
+    param.startTimer = true;
+    Timer_A_initUpMode(TIMER_A0_BASE, &param);
 
-typedef struct {
-    char command[20];
-    char parameters[3][10];
-} Command;
+    // Change timer delay to 1/8 second
+    Timer_A_setCompareValue(TIMER_A0_BASE,
+                            TIMER_A_CAPTURECOMPARE_REGISTER_0,
+                            0x1000
+                            );
 
-EVR_Code EVR(const char *format, ...) {
-    if (format == NULL) {
-        return EVR_NULL_ERROR;
-    }
+    //Enable/Start sampling and conversion
+    /*
+     * Base address of ADC12B Module
+     * Start the conversion into memory buffer 0
+     * Use the single-channel, single-conversion mode
+     */
+    ADC12_B_startConversion(ADC12_B_BASE,
+                            ADC12_B_MEMORY_0,
+                            ADC12_B_SINGLECHANNEL);
 
-    EVR_Code ret = EVR_SUCCESS;
-    char buffer[UART_BUFFER_SIZE];
-    va_list args;
+    while (ADC12_B_isBusy(ADC12_B_BASE) == ADC12BUSY);
 
-    // Start variadic arguments processing
-    va_start(args, format);
-
-    // Format the string into the buffer
-    if (vsnprintf(buffer, UART_BUFFER_SIZE, format, args) >= UART_BUFFER_SIZE) {
-        ret = EVR_ERROR; // Buffer overflow error
-    }
-
-    // End variadic arguments processing
-    va_end(args);
-
-    if (ret == EVR_SUCCESS) {
-        const char *message = buffer;
-        while (*message) {
-            EUSCI_A_UART_transmitData(EUSCI_A1_BASE, *message++);
-            while (!EUSCI_A_UART_getInterruptStatus(EUSCI_A1_BASE, EUSCI_A_UART_TRANSMIT_INTERRUPT_FLAG));
-        }
-    }
-
-    return ret;
+    tempADCCombined=(ADC12MEM0_H<<8) | ADC12MEM0_L;
+    EVR("ADC Temp Combine: %d\n\r", tempADCCombined);
 }
 
-EVR_Code GetCommand(Command *cmd) {
-    if (cmd == NULL) {
-        return EVR_NULL_ERROR;
-    }
-
-    EVR_Code ret = EVR_SUCCESS;
-    char buffer[50];
-    int index = 0;
-
-    // Read characters until newline or buffer full
-    while (index < sizeof(buffer) - 1) {
-        while (!EUSCI_A_UART_getInterruptStatus(EUSCI_A1_BASE, EUSCI_A_UART_RECEIVE_INTERRUPT_FLAG));
-        char c = EUSCI_A_UART_receiveData(EUSCI_A1_BASE);
-
-        if (c == '\n' || c == 0x13) { // Ends at carriage return
-            buffer[index] = '\0';
-            break;
-        }
-        buffer[index++] = c;
-    }
-
-    // Ensure null termination if buffer limit is reached
-    buffer[sizeof(buffer) - 1] = '\0';
-
-    // Parse the command
-    char *token = strtok(buffer, " ");
-    if (token) {
-
-        // Command too long
-        if (strlen(token) >= sizeof(cmd->command)) {
-            return EVR_ERROR;
-        }
-        memcpy(cmd->command, token, sizeof(cmd->command) - 1);
-        cmd->command[sizeof(cmd->command) - 1] = '\0';
-
-        int count = 0;
-        while ((token = strtok(NULL, " ")) && count < 3) {
-            char *end = token + strlen(token) - 1;
-            while (end >= token && (*end == '\r' || *end == '\n')) {
-                *end = '\0';
-                end--;
-            }
-
-            // Parameter too long
-            if (strlen(token) >= sizeof(cmd->parameters[0])) {
-                return EVR_ERROR; 
-            }
-
-            memcpy(cmd->parameters[count], token, sizeof(cmd->parameters[0]) - 1);
-            cmd->parameters[count][sizeof(cmd->parameters[0]) - 1] = '\0';
-            count++;
-        }
-
-        // Cmd Response
-        char response[100];
-        snprintf(response, sizeof(response), "Called %s %s %s %s\n\r",
-                 cmd->command,
-                 count > 0 ? cmd->parameters[0] : "",
-                 count > 1 ? cmd->parameters[1] : "",
-                 count > 2 ? cmd->parameters[2] : "");
-        EVR(response);
-    } else {
-        ret = EVR_ERROR;
-    }
-
-    return ret;
-}
-
-
-void main(void) {
-    // Stop watchdog timer
+void main (void)
+{
+    //Stop WDT
     WDT_A_hold(WDT_A_BASE);
-
-    // Unlock GPIO functionality
     PMM_unlockLPM5();
 
-    // Configure UART pins (P2.6 -> TXD, P2.5 -> RXD)
     GPIO_setAsPeripheralModuleFunctionInputPin(
-        GPIO_PORT_P2,
-        GPIO_PIN6 | GPIO_PIN5,
-        GPIO_SECONDARY_MODULE_FUNCTION
+        GPIO_PORT_P1,
+        GPIO_PIN3,
+        GPIO_TERNARY_MODULE_FUNCTION
     );
 
-    // Error R LED
-    GPIO_setAsOutputPin(GPIO_PORT_P4, GPIO_PIN6);
-
-    // Initialize UART
-    if (STATUS_FAIL == EUSCI_A_UART_init(EUSCI_A1_BASE, &uartConfig)) {
-        GPIO_setOutputHighOnPin(GPIO_PORT_P4, GPIO_PIN6);
-        return;
-    }
-
-    // Enable UART module
-    EUSCI_A_UART_enable(EUSCI_A1_BASE);
-    EVR("System is running...\n\r");
+    EVR_Init(&evr);
+    ADC_Init();
 
     while (1) {
-        Command cmd = {0};
-        GetCommand(&cmd);
+        GetAdcValue();
+        __delay_cycles(50000);
     }
+}
+
+void ADC_Init(void)
+{
+    //Initialize the ADC12B Module
+    /*
+     * Base address of ADC12B Module
+     * Use internal ADC12B bit as sample/hold signal to start conversion
+     * USE MODOSC 5MHZ Digital Oscillator as clock source
+     * Use default clock divider/pre-divider of 1
+     * Use Temperature Sensor and Battery Monitor internal channels
+     */
+    ADC12_B_initParam initParam = {0};
+    initParam.sampleHoldSignalSourceSelect = ADC12_B_SAMPLEHOLDSOURCE_SC;
+    initParam.clockSourceSelect = ADC12_B_CLOCKSOURCE_ADC12OSC;
+    initParam.clockSourceDivider = ADC12_B_CLOCKDIVIDER_1;
+    initParam.clockSourcePredivider = ADC12_B_CLOCKPREDIVIDER__1;
+    initParam.internalChannelMap = ADC12_B_NOINTCH;
+    ADC12_B_init(ADC12_B_BASE, &initParam);
+
+    // Enable the ADC12B module
+    ADC12_B_enable(ADC12_B_BASE);
+
+    // Sets up the sampling timer pulse mode
+    ADC12_B_setupSamplingTimer(ADC12_B_BASE,
+                               ADC12_B_CYCLEHOLD_128_CYCLES,
+                               ADC12_B_CYCLEHOLD_128_CYCLES,
+                               ADC12_B_MULTIPLESAMPLESDISABLE);
+
+    // Maps Temperature Sensor input channel to Memory 0 and select voltage references
+    /*
+     * Base address of the ADC12B Module
+     * Configure memory buffer 0
+     * Map input A1 to memory buffer 0
+     * Vref+ = IntBuffer
+     * Vref- = AVss
+     * Memory buffer 0 is not the end of a sequence
+     */
+    ADC12_B_configureMemoryParam configureMemoryParam = {0};
+    configureMemoryParam.memoryBufferControlIndex = ADC12_B_MEMORY_0;
+    configureMemoryParam.inputSourceSelect = ADC12_B_INPUT_A3;
+    configureMemoryParam.refVoltageSourceSelect = ADC12_B_VREFPOS_INTBUF_VREFNEG_VSS;
+    configureMemoryParam.endOfSequence = ADC12_B_NOTENDOFSEQUENCE;
+    configureMemoryParam.windowComparatorSelect = ADC12_B_WINDOW_COMPARATOR_DISABLE;
+    configureMemoryParam.differentialModeSelect = ADC12_B_DIFFERENTIAL_MODE_DISABLE;
+    ADC12_B_configureMemory(ADC12_B_BASE, &configureMemoryParam);
+
+    // Clear memory buffer 0 interrupt
+    ADC12_B_clearInterrupt(ADC12_B_BASE, 0, ADC12_B_IFG0);
+
+    // Enable memory buffer 0 interrupt
+    ADC12_B_enableInterrupt(ADC12_B_BASE, ADC12_B_IE0, 0, 0);
+
+    // Configure internal reference
+    while(Ref_A_isRefGenBusy(REF_A_BASE));              // If ref generator busy, WAIT
+    Ref_A_enableTempSensor(REF_A_BASE);
+    Ref_A_setReferenceVoltage(REF_A_BASE, REF_A_VREF2_5V);
+    Ref_A_enableReferenceVoltage(REF_A_BASE);
+}
+
+/*
+ * ADC12 Interrupt Service Routine
+ * Exits LPM3 when Temperature/Voltage data is ready
+ */
+#pragma vector = ADC12_VECTOR
+__interrupt void ADC12_ISR(void)
+{
+  switch(__even_in_range(ADC12IV,76))
+  {
+    case  ADC12IV_NONE: break;                // Vector  0:  No interrupt
+    case  ADC12IV_ADC12OVIFG: break;          // Vector  2:  ADC12MEMx Overflow
+    case  ADC12IV_ADC12TOVIFG: break;         // Vector  4:  Conversion time overflow
+    case  ADC12IV_ADC12HIIFG: break;          // Vector  6:  ADC12HI
+    case  ADC12IV_ADC12LOIFG: break;          // Vector  8:  ADC12LO
+    case ADC12IV_ADC12INIFG: break;           // Vector 10:  ADC12IN
+    case ADC12IV_ADC12IFG0:                   // Vector 12:  ADC12MEM0
+        ADC12IFGR0 &= ~ADC12IFG0;             // Clear interrupt flag
+        __bic_SR_register_on_exit(LPM3_bits); // Exit active CPU
+        break;
+    case ADC12IV_ADC12IFG1:                   // Vector 14:  ADC12MEM1
+        ADC12IFGR0 &= ~ADC12IFG1;             // Clear interrupt flag
+        __bic_SR_register_on_exit(LPM3_bits); // Exit active CPU
+        break;
+    case ADC12IV_ADC12IFG2: break;            // Vector 16:  ADC12MEM2
+    case ADC12IV_ADC12IFG3: break;            // Vector 18:  ADC12MEM3
+    case ADC12IV_ADC12IFG4: break;            // Vector 20:  ADC12MEM4
+    case ADC12IV_ADC12IFG5: break;            // Vector 22:  ADC12MEM5
+    case ADC12IV_ADC12IFG6: break;            // Vector 24:  ADC12MEM6
+    case ADC12IV_ADC12IFG7: break;            // Vector 26:  ADC12MEM7
+    case ADC12IV_ADC12IFG8: break;            // Vector 28:  ADC12MEM8
+    case ADC12IV_ADC12IFG9: break;            // Vector 30:  ADC12MEM9
+    case ADC12IV_ADC12IFG10: break;           // Vector 32:  ADC12MEM10
+    case ADC12IV_ADC12IFG11: break;           // Vector 34:  ADC12MEM11
+    case ADC12IV_ADC12IFG12: break;           // Vector 36:  ADC12MEM12
+    case ADC12IV_ADC12IFG13: break;           // Vector 38:  ADC12MEM13
+    case ADC12IV_ADC12IFG14: break;           // Vector 40:  ADC12MEM14
+    case ADC12IV_ADC12IFG15: break;           // Vector 42:  ADC12MEM15
+    case ADC12IV_ADC12IFG16: break;           // Vector 44:  ADC12MEM16
+    case ADC12IV_ADC12IFG17: break;           // Vector 46:  ADC12MEM17
+    case ADC12IV_ADC12IFG18: break;           // Vector 48:  ADC12MEM18
+    case ADC12IV_ADC12IFG19: break;           // Vector 50:  ADC12MEM19
+    case ADC12IV_ADC12IFG20: break;           // Vector 52:  ADC12MEM20
+    case ADC12IV_ADC12IFG21: break;           // Vector 54:  ADC12MEM21
+    case ADC12IV_ADC12IFG22: break;           // Vector 56:  ADC12MEM22
+    case ADC12IV_ADC12IFG23: break;           // Vector 58:  ADC12MEM23
+    case ADC12IV_ADC12IFG24: break;           // Vector 60:  ADC12MEM24
+    case ADC12IV_ADC12IFG25: break;           // Vector 62:  ADC12MEM25
+    case ADC12IV_ADC12IFG26: break;           // Vector 64:  ADC12MEM26
+    case ADC12IV_ADC12IFG27: break;           // Vector 66:  ADC12MEM27
+    case ADC12IV_ADC12IFG28: break;           // Vector 68:  ADC12MEM28
+    case ADC12IV_ADC12IFG29: break;           // Vector 70:  ADC12MEM29
+    case ADC12IV_ADC12IFG30: break;           // Vector 72:  ADC12MEM30
+    case ADC12IV_ADC12IFG31: break;           // Vector 74:  ADC12MEM31
+    case ADC12IV_ADC12RDYIFG: break;          // Vector 76:  ADC12RDY
+    default: break;
+  }
 }
