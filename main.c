12a378167683b2c144c15441fa334e32d48f34b9
Chrisx19 <christopherlazo1619@gmail.com>
2026-01-04 23:13:05 -0800

    main is finalized

--- main.c
+++ main.c
@@ -1,10 +1,14 @@
-#include "driverlib.h"
+#include "humidity_sensor_sht31.h"
 #include "evr.h"
+#include "driverlib.h"
 
-void main (void)
+static Hum_Sensor_t hum = {0};
+
+void main(void)
 {
-    //Stop WDT
+    //Stop watchdog timer
     WDT_A_hold(WDT_A_BASE);
+    PMM_unlockLPM5();
 
     /* Set DCO to 8 MHz */
     CS_setDCOFreq(CS_DCORSEL_1, CS_DCOFSEL_3);
@@ -19,9 +23,23 @@ void main (void)
         EVR("EVR Initialized.");
     }
 
-     while (1)
-    {
-    	__bis_SR_register(LPM0_bits); // CPU off
+    if (Hum_Init(&hum) != HUM_STATUS_SUCCESS) {
+        __bis_SR_register(LPM0_bits); // CPU off
         __no_operation(); // Remain in LPM0
+    } else {
+        EVR("Humidity Initialized.");
+    }
+
+    __bis_SR_register(GIE); //enable interrupts
+
+    while(1) {
+        if (Hum_Get_Humidity(&hum) != HUM_STATUS_SUCCESS) {
+            __bis_SR_register(LPM0_bits); // CPU off
+            __no_operation(); // Remain in LPM0
+        } else {
+            EVR("Humidity: %.02f%", hum.humidity);
+        }
+
+        __delay_cycles(8000000); // ~1 second @ 8 MHz
     }
 }
